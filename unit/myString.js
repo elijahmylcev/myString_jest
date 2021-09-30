a98b13141d635b4a35082bc9b6e9dc81e3d1c78a
Elijah <elijahmylcev@gmail.com>
2021-09-30 22:25:26 +0300

    create class MyString

--- unit/myString.js
+++ unit/myString.js
@@ -1,6 +1,19 @@
-class myString {
+class MyString extends String {
     
+    has(substring) {
+        return this.includes(substring) //true or false
+    }
 
+    remove(char) {
+        const i = this.indexOf(char);
+        
+        if (i == -1) {
+            return null
+        } else {
+            this.remove(i)
+            return i
+        }
+    }
 }
 
-module.exports = {myString};
+module.exports = {MyString};
--- unit/myString.test.js
+++ unit/myString.test.js
@@ -1,21 +1,13 @@
-const {myString} = require('./myString');
+const {MyString} = require('./myString');
 
-const str = new myString(['h', 'e', 'l', 'l']);
 
 describe('myString', () => {
-    
+    const str = new MyString('hell');
+
     beforeEach(() => {
         strTest = 'two steps from '
     });
 
-    test.todo('Constructor must return string');
-    test.todo('must support string methods, for example String.concat(...) ');
-    test.todo('Has checks whether the passed substring is contained in the current string. Returns true or false ');
-    test.todo('Remove deletes the first occurrence of the passed character and returns the index of the deleted character or null ');
-
-    test('Constructor must return string', () => {
-        expect(str === String.prototype).toBeTruthy();
-    });
 
     test('must support string methods, for example String.concat(...)', () => {
         expect(strTest.concat(str)).toEqual('two steps from hell');
